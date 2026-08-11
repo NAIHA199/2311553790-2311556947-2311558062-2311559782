fe7b812e1fb168437b9f14625fe9128091b808ae
nlua1 <nlua1965@gmail.com>
2026-08-11 21:59:28 +0700

    thêm landing

--- LibraryAdvanced/Program.cs
+++ LibraryAdvanced/Program.cs
@@ -37,7 +37,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
-
+    pattern: "{controller=Home}/{action=Landing}/{id?}");
 
 app.Run();
