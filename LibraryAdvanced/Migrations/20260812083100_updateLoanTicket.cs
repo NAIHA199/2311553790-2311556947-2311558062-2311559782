c39d5271f07b58253f30d49da1ec0300ab04acd6
hoangphuc <dhoangphuc1007@gmail.com>
2026-08-12 15:54:35 +0700

    Thêm quản lí người dùng

---
+++ LibraryAdvanced/Migrations/20260812083100_updateLoanTicket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryAdvanced.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateLoanTicket : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "LoanTickets",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReturnedDate",
+                table: "LoanTickets",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "LoanTickets");
+
+            migrationBuilder.DropColumn(
+                name: "ReturnedDate",
+                table: "LoanTickets");
+        }
+    }
+}
